01e0b6b042fe10782c056b9dda64eb74a1c3d33d
Spelaia58 <Spelaia58@gmail.com>
2020-04-13 14:39:21 +0300

    w11

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import './styles.css';
 
-
-colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
 const refs = {
-    startBtn: document.querySelector('button[data-action="start"'),
-    stopBtn: document.querySelector('button[data-action="stop"'),
-  },
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"'),
+};
 
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
- 
-  let interval = null;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const setRandomColor = () => {
+  const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
 
-  refs.startBtn.addEventListener('click', e => interval = interval ? interval:
-  setInterval(() => randomIntegerFromInterval(), 1000));
+  document.body.style.backgroundColor = randomColor;
+};
 
-  refs.stopBtn.addEventListener('click', e => clearInterval(interval));
+let interval = null;
+
+refs.startBtn.addEventListener(
+  'click',
+  e =>
+    (interval = interval
+      ? interval
+      : setInterval(() => setRandomColor(), 1000)),
+);
+
+refs.stopBtn.addEventListener('click', e => clearInterval(interval));
